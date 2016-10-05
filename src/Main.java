ba7613d833db23d6eb6a1fbe9cb55c1f04225563
Hyunjae <guswoqkrtk@gmail.com>
2016-10-05 20:24:07 +0900

    Add HTML output

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String file = "Noitamina.txt";
-
         TreeMap<String, LinkedHashMap<String, String>> map = new TreeMap<>();
 
-        Set<SmallMap> threadSet = new HashSet<>();
+        List<SmallMap> threadList = new ArrayList<>();
         BigMap.bigMap().forEach((bigTitle, bigUrl) -> {
             LinkedHashMap<String, String> smallMap = new LinkedHashMap<>();
             map.put(bigTitle, smallMap);
             SmallMap smallMapThread = new SmallMap(bigUrl, smallMap);
-            threadSet.add(smallMapThread);
+            smallMapThread.setName(bigTitle);
+            threadList.add(smallMapThread);
             smallMapThread.start();
         });
 
@@ -33,46 +32,68 @@ public class Main {
             threadSet.add(smallMapThread);
             smallMapThread.start();
             i++;
-            if(i == 50)
+            if(i == 5)
                 break;
         }
         */
 
-        threadSet.forEach(t -> {
+        threadList.forEach(t -> {
             try {
                 t.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                //System.out.println(e.getMessage());
             }
         });
 
+        String file = "Noitamina.html";
+
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+
+            bufferedWriter.write(
+                    "<!DOCTYPE HTML>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "\t<meta charset=\"UTF-8\">\n" +
+                    "\t<title>Noitamina</title>\n" +
+                    "</head>\n" +
+                    "<body>\n"
+            );
 
             for (Map.Entry<String, LinkedHashMap<String, String>> bigEntry : map.entrySet()) {
 
                 System.out.println(bigEntry.getKey());
 
-                bufferedWriter.write(bigEntry.getKey());
-                bufferedWriter.newLine();
+                bufferedWriter.write("\t<h1>" + bigEntry.getKey() + "</h1>\n");
+
+                 /* txt output
+                    bufferedWriter.write(bigEntry.getKey());
+                    bufferedWriter.newLine();
+                    */
 
                 for (Map.Entry<String, String > smallEntry: bigEntry.getValue().entrySet()) {
 
                     System.out.println(smallEntry.getKey() + " | " + smallEntry.getValue());
 
+                    bufferedWriter.write("\t<p><a href=\"" + smallEntry.getValue() + "\">" + smallEntry.getKey() + "</a></p>\n");
+
+                    /* txt output
                     bufferedWriter.write(smallEntry.getKey() + " | " + smallEntry.getValue());
                     bufferedWriter.newLine();
+                    */
 
                 }
 
             }
 
+            bufferedWriter.write(
+                    "</body>\n" +
+                    "</html>\n"
+            );
 
             bufferedWriter.close();
 
         } catch (Exception e) {
             e.printStackTrace();
-            //System.out.println(e.getMessage());
         }
 
 
