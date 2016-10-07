16cbc0855af58076f1e37193a0542449a2365a37
Hyunjae <guswoqkrtk@gmail.com>
2016-10-08 05:53:18 +0900

    Change variables

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,18 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    private static final String NOITAMINA_JSON = "Noitamina.json";
+    private static final String NOITAMINA_MD5 = "Noitamina.md5";
+    private static final String BIGMAP_MD5 = "BigMap.md5";
+
     public static void main(String[] args) {
 
         // Deserialization
 
         Map<String, Map<String, String>> map = null;
 
-        String jsonFile = "Noitamina.json";
-
         try {
-            map = IO.readJson(jsonFile, TreeMap.class);
+            map = IO.readJson(NOITAMINA_JSON, TreeMap.class);
         } catch(Exception e) {
             logger.info(e.getMessage());
         }
@@ -28,14 +30,16 @@ public class Main {
 
         // Thread Pool
 
-        if(!MD5.read("Noitamina.md5").equals(MD5.fileToMD5("Noitamina.json")) || !MD5.read("BigMap.md5").equals(BigMap.md5()) || map.size() == 0) {
+        if(!MD5.read(NOITAMINA_MD5).equals(MD5.fileToMD5(NOITAMINA_JSON)) ||
+                !MD5.read(BIGMAP_MD5).equals(BigMap.md5()) ||
+                map.size() == 0) {
             Future.future(map);
         }
 
         // Serialization
 
         try {
-            IO.writeJson(jsonFile, map);
+            IO.writeJson(NOITAMINA_JSON, map);
         } catch(Exception e) {
             logger.error(e.getMessage(), e);
         }
@@ -46,8 +50,8 @@ public class Main {
 
         // MD5
 
-        MD5.write("Noitamina.md5", MD5.fileToMD5("Noitamina.json"));
-        MD5.write("BigMap.md5", BigMap.md5());
+        MD5.write(NOITAMINA_MD5, MD5.fileToMD5(NOITAMINA_JSON));
+        MD5.write(BIGMAP_MD5, BigMap.md5());
 
     }
 
